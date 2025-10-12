c57595ee710556e6fad2be121e87d6f82287a2ef
Federico <federicohernansanches@gmail.com>
2025-10-12 18:05:43 -0300

    Agregar funcionalidad para leer argumentos desde la línea de comandos en la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,18 @@
+import argparse
+
 def suma(a,b):
     return a + b
 
 def main():
-    print("Suma de 2 + 3 =", suma(2, 3))
+    # Leer los argumentos
+    parser = argparse.ArgumentParser(description="Calculadora simple por línea de comandos.")
+    parser.add_argument("num1", type=float, help="Primer número")
+    parser.add_argument("num2", type=float, help="Segundo número")
+    parser.add_argument("--op", required=True, choices=["suma"], help="Operación a realizar")
+
+    args = parser.parse_args()
+
+    print(suma(args.num1, args.num2))
 
 if __name__ == "__main__":
     main()
